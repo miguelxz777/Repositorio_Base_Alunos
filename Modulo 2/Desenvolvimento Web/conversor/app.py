278c7c8f447e239e2db30ab3e7ac96083ef2b2ac
miguelxz777 <miguelxz77701@gmail.com>
2025-11-13 11:26:56 -0300

    conversor monetario
    app.py,index

---
+++ Modulo 2/Desenvolvimento Web/conversor/app.py
@@ -0,0 +1,12 @@
+from flask import Flask,request, render_template
+
+app = Flask(__name__)
+@app.route("/", methods=['GET'])
+def index():
+    if request.method =='GET':
+        valor1 = float(request.args.get('valor1',0))
+        cotação_do_dolar = 5.29
+        resultado = valor1 * cotação_do_dolar 
+        return render_template('index.html', valor1=valor1, resultado=resultado)
+    else:
+        return 'h1 color="red"> Erro</h1>' 
